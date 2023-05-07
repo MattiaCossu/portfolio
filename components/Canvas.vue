a66cfad1d17892b07c7593fab2b8bb84823b465c
MattiaCossu <cossumattia5@gmail.com>
2023-05-07 16:01:14 +0200

    Component to manage the canvas in the hero

---
+++ components/Canvas.vue
@@ -0,0 +1,48 @@
+<template>
+    <div ref="el" class="container" @resize="onResize">
+        <canvas ref="canvas"></canvas>
+    </div>
+</template>
+  
+<script setup lang="ts">
+const el = ref<HTMLDivElement | null>(null)
+const canvas = ref<HTMLCanvasElement | null>(null)
+useRainEffect(canvas, el);
+
+const onResize = () => {
+    if (canvas.value && el.value) {
+        canvas.value.width = el.value.clientWidth
+        canvas.value.height = el.value.clientHeight
+    }
+};
+
+onMounted(() => {
+    window.addEventListener('resize', onResize)
+    onResize();
+});
+
+onUnmounted(() => {
+    window.removeEventListener('resize', onResize)
+});
+</script>
+  
+<style>
+html,
+body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
+
+.container {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    overflow: hidden;
+}
+
+canvas {
+    display: block;
+}
+</style>
+  
